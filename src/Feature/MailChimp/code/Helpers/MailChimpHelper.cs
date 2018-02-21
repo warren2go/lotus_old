0303caeff4ad609101e7ea79b85f4581b7517544
warren2go <warren2go@gmail.com>
2018-02-21 15:25:22 +1100

    Added MailChimp helper to structure.
    Signed-off-by: warren2go <warren2go@gmail.com>

---
+++ src/Feature/MailChimp/code/Helpers/MailChimpHelper.cs
@@ -0,0 +1,12 @@
+﻿using Lotus.Foundation.Extensions.RegularExpression;
+
+namespace Lotus.Feature.MailChimp.Helpers
+{
+    public static class MailChimpHelper
+    {
+        public static string FilterDateTime(string date, string @default = "01/01/1900")
+        {
+            return string.IsNullOrEmpty(date) ? @default : date.ReplacePattern(@"[/\- ]", @"/");
+        }
+    }
+}
