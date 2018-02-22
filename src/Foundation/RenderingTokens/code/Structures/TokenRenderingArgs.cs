aa27d86f4c8eb3b1cf93ee62945f030b178bdd7f
warren2go <warren2go@gmail.com>
2018-02-22 14:09:58 +1100

    Resolved RenderingTokens foundation.
    Signed-off-by: warren2go <warren2go@gmail.com>

---
+++ src/Foundation/RenderingTokens/code/Structures/TokenRenderingArgs.cs
@@ -0,0 +1,13 @@
+﻿using Lotus.Foundation.RenderingTokens.Helpers;
+using Sitecore.Pipelines;
+
+namespace Lotus.Foundation.RenderingTokens.Structures
+{
+    public class TokenRenderingArgs : PipelineArgs
+    {
+        public void Add(object model)
+        {
+            CustomData.Add(RenderingTokensHelper.ResolveTokenName(model), model);
+        }
+    }
+}
